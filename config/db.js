9d88626c839805f15185ef1c20ce8a890a1021fb
Cris john <kris.ocay@gmail.com>
2025-10-14 16:24:31 +0800

    updated db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
 
-const connect = async () => {
+const connect = async (uri) => {
+    const connectionString = uri || process.env.MONGO_URI || 'mongodb+srv://ayawsir:Timogan00@database.vyu7sev.mongodb.net/?retryWrites=true&w=majority&appName=Database';
     try {
-        const conn = await mongoose.connect('mongodb+srv://ayawsir:Timogan00@database.vyu7sev.mongodb.net/?retryWrites=true&w=majority&appName=Database', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        // Fail faster on DNS/network issues with a short serverSelectionTimeoutMS
+        const conn = await mongoose.connect(connectionString, { serverSelectionTimeoutMS: 5000 });
         console.log('mongoDB connected', conn.connection.host);
     } catch (err) {
         console.error('MongoDB connection error:', err && err.message ? err.message : err);
