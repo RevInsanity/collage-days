6cd8d4d091475e21b0f758935dc71d2b3d50666e
Cris john <kris.ocay@gmail.com>
2025-10-14 14:56:47 +0800

    update server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-app.use('/api/user', userRoutes)
+app.use('/api/users', userRoutes)
 
 
 app.listen(PORT, () => {
